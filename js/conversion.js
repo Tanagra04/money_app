f93e8b7db8e8996ea0c1de95935235b859fb707e
Tanagra04 <vanegasrojas29@gmail.com>
2023-07-28 19:20:30 -0500

    280723

--- js/conversion.js
+++ js/conversion.js
@@ -40,3 +40,71 @@ function populteOptions(){
     
 
 }
+
+// Crear una función para la operación de conversión
+/**
+ * 
+ * @param {Number} val es valor
+ * @param {Number} fromCurr moneda inicial
+ * @param {Number} toCurr moneda a convertir
+ */
+function convert(val,fromCurr,toCurr){
+    // declarar una variables locales
+    let v = (val/rates[fromCurr]) * rates[toCurr];
+    let v1 = v.toFixed(3);
+    // validar con un if ternario
+    return v1 ==0.0 ? v.toFixed(5) : v1;
+
+}
+
+// crear la función para conocer las tasas de conversión
+function displayRate(){
+    // declarar variables locales
+    let v1 = sel1.value;
+    let v2 = sel2.value;
+
+    // imprimir los valores en los elementos HTML
+    let val = convert(1, v1 , v2);
+    rate1.innerHTML = `1 ${v1} equals-(valor)`;
+    rate2.innerHTML = `${val} ${v2}`;
+
+}
+
+// listener o evento click del botón, con una función de flecha () =>
+resultBtn.addEventListener('click', () =>{
+    // declarar variables locales
+    let fromCurr = sel1.value;
+    let fromVal = parseFloat(inpt1.value);
+    let toCurr = sel2.value;
+
+    // validar con un condicional if, si hay o no un número y mostrar una alerta
+    if (isNaN(fromVal)) {
+        alert('Ingresar un número, por favor');
+    }else{
+        let cVal = convert(fromVal,fromCurr,toCurr);
+        inpt2.value = cVal;
+    }
+
+});
+
+// listener para realizar conversión inversa
+
+selects.forEach(s => s.addEventListener("change",displayRate));
+
+document.querySelector('.swap').addEventListener("click", () => {
+    //declarar variables
+    let in1 = inpt1.value;
+    let in2 = inpt2.value;
+    let op1 = sel1.value;
+    let op2 = sel2.value;
+
+    inpt2.value = in1;
+    inpt1.value = in2;
+
+    sel2.value = op1;
+    sel1.value = op2;
+
+
+    //utilizar la funcion de la seleccion de los valores de monedas
+    displayRate();
+})
